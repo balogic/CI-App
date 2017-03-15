f8d5f110e0f7eff643dc0cd2baede287ec62e42b
Balaji <rbalajives@gmail.com>
2017-03-15 12:28:37 +0530

    changed test cases

--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class WelcomeControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get welcome_home_url
+    post welcome_home_url
     assert_response :success
   end
 
